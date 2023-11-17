9a8aecbb3894bab86bfe14778ae27de37ee091fb
MudassarA10 <123820684+MudassarA10@users.noreply.github.com>
2023-11-17 18:08:03 +0500

    commit change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/MudassarA10/TFT_Clone.git",
+  base: "https://mudassara10.github.io/TFT_Clone/",
   plugins: [react()],
 })
